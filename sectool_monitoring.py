4b2a68afb271d5ebe216dbfe583777507a5d8eb1
test <test>
2022-11-23 23:07:17 +0800

    1

--- sectool_monitoring.py
+++ sectool_monitoring.py
@@ -134,13 +134,12 @@ for type_1 in data:
     md += '### {}\n'.format(type_1)
     for type_2 in data[type_1]:
         md += '#### {}\n'.format(type_2)
+        md += '| 项目名称 | 作者 | 最近提交时间 | 版本 | 项目描述 |\n'
+        md += '| :---- | :---- | :---- | :---- | :---- |\n'
         for url in data[type_1][type_2]:
-            md += '| 项目名称 | 作者 | 最近提交时间 | 版本 | 项目描述 |\n'
-            md += '| :---- | :---- | :---- | :---- | :---- |\n'
-            for url in data[type_1][type_2]:
-                author, name = url[19:].split('/', 1)
-                md += '| [{}]({}) | {} | {} | {} | {} |\n'.format(name, url, author, data[type_1][type_2][url].get('commit_date',
-                                                                                                                   ''), data[type_1][type_2][url].get('release_version', ''), data[type_1][type_2][url].get('description', '').replace('\r\n', '<br>').replace('\n', '<br>'))
+            author, name = url[19:].split('/', 1)
+            md += '| [{}]({}) | {} | {} | {} | {} |\n'.format(name, url, author, data[type_1][type_2][url].get('commit_date',
+                                                                                                                ''), data[type_1][type_2][url].get('release_version', ''), data[type_1][type_2][url].get('description', '').replace('\r\n', '<br>').replace('\n', '<br>'))
 
 with open("README.md", 'w', encoding='utf8') as fd:
     fd.write(md)
