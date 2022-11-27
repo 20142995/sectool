9e64666db97960e6337a90a63611f0256cf7eef3
test <test>
2022-11-27 18:26:25 +0800

    1

--- run.py
+++ run.py
@@ -5,17 +5,100 @@ import datetime
 import time
 import os
 import json
-import sys
 import traceback
-sys.dont_write_bytecode = True
-from github_help import GithubClient
+import time
+import requests
+
+requests.packages.urllib3.disable_warnings()
+
+class GithubClient:
+
+    def __init__(self, token):
+        self.url = 'https://api.github.com'
+        self.headers = {
+            'Authorization': 'Bearer {}'.format(token),
+            'Connection': 'close',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'
+        }
+        self.limit = 0
+        self.users_octocat()
+
+    def connect(self, method, resource, data=None):
+        '''访问api'''
+        time.sleep(0.1)
+        if method == 'GET':
+            r = requests.get('{0}{1}'.format(
+                self.url, resource), params=data, headers=self.headers, verify=False, allow_redirects=False)
+        elif method == 'POST':
+            r = requests.post('{0}{1}'.format(
+                self.url, resource), data=data, headers=self.headers, verify=False, allow_redirects=False)
+        r.encoding = r.apparent_encoding
+        if 'X-RateLimit-Remaining' in r.headers.keys():
+            self.limit = int(r.headers['X-RateLimit-Remaining'])
+        try:
+            return r.status_code, r.headers, r.json()
+        except:
+            return r.status_code, r.headers, r.content
+
+    def search_code(self, keyword, page=1, per_page=10):
+        '''搜索代码'''
+        try:
+            time.sleep(2)
+            data = {'q': keyword, 'sort': 'indexed',
+                    'order': 'desc', 'page': page, 'per_page': per_page}
+            _, _, rs = self.connect("GET", '/search/code', data=data)
+            return rs
+        except:
+            return {}
+
+    def repos(self, author, repo):
+        '''项目信息'''
+        try:
+            _, _, rs = self.connect("GET", '/repos/{}/{}'.format(author, repo))
+            return rs
+        except:
+            return {}
+
+    def repos_commits(self, author, repo):
+        '''项目commit信息'''
+        try:
+            _, _, rs = self.connect(
+                "GET", '/repos/{}/{}/commits'.format(author, repo))
+            if isinstance(rs, dict):
+                if rs.get('message', '') == 'Moved Permanently' and 'url' in rs:
+                    _, _, rs1 = self.connect("GET", rs['url'][18:])
+                    if isinstance(rs1, list):
+                        return rs1
+            elif isinstance(rs, list):
+                return rs
+        except:
+            pass
+        return []
+
+    def repos_releases_latest(self, author, repo):
+        '''项目最新release'''
+        try:
+            _, _, rs = self.connect(
+                "GET", '/repos/{}/{}/releases/latest'.format(author, repo))
+            return rs
+        except:
+            return {}
+
+    def users_octocat(self):
+        '''检查速率限制'''
+        try:
+            _, _, _ = self.connect(
+                "GET", '/users/octocat')
+        except:
+            pass
+
 
 # 保存数据
 data = {}
 data_file = 'data.json'
 
 # 项目清单
-with open('repos.md','r',encoding='utf8') as fr:
+with open('repos.md', 'r', encoding='utf8') as fr:
     repos = fr.readlines()
     for repo in repos[2:]:
         if '|' in repo:
@@ -30,6 +113,7 @@ with open('repos.md','r',encoding='utf8') as fr:
             data.setdefault(type_1, {})
             data[type_1].setdefault(type_2, {})
             data[type_1][type_2].setdefault(url, {})
+        break
 
 # 更新数据
 gc = GithubClient(os.getenv('GH_TOKEN'))
@@ -104,7 +188,8 @@ for type_1 in data:
                     if time.mktime(time.strptime(commit_date, "%Y-%m-%d %H:%M:%S")) > time.mktime((datetime.datetime.now() - datetime.timedelta(days=n)).timetuple()):
                         commit_md += '| {} | [{}]({}) | {} | {} |\n'.format(
                             type_2, repo, url, commit_date, commit_message.replace('\r\n', '<br>').replace('\n', '<br>'))
-                total_md += '| [{}]({}) | {} | {} | {} |\n'.format(repo, url,commit_date, release_tag, description.replace('\r\n', '<br>').replace('\n', '<br>'))
+                total_md += '| [{}]({}) | {} | {} | {} |\n'.format(repo, url, commit_date,
+                                                                   release_tag, description.replace('\r\n', '<br>').replace('\n', '<br>'))
             except:
                 print('[fail 2]', url)
                 traceback.print_exc()
