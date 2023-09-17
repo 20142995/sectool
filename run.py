f15384cb7a0199cce53fdb3b953c2c810467f62d
John Doe <johndoe@example.com>
2023-09-17 10:20:34 +0800

    1

--- run.py
+++ run.py
@@ -156,7 +156,9 @@ def main():
     else:
         with open(data_file, "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
+    # 跳过commit_date为空的
     data_b = {url: data[url] for url in data if data[url].get("commit_date", "") != ""}
+    # 跳过commit_date距今超过180天的
     data_b = {
         url: data_b[url]
         for url in data_b
